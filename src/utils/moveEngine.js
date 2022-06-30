553b6ae8959929999c11421810cfef3f9248e967
Patrick Bull <patrickb@olmgroup.com>
2022-06-30 22:18:38 +0100

    added rook movement

--- src/utils/moveEngine.js
+++ src/utils/moveEngine.js
@@ -7,28 +7,37 @@ export const generateAllMoves = (boardState) => {
   }
 };
 
-export const generateMoves = ({ tiles }, { piece, row, col }) => {
+export const generateMoves = (
+  { tiles },
+  { piece, row: pieceRow, col: pieceCol }
+) => {
   const validMoves = [];
   switch (piece.type) {
     case PieceType.PAWN:
-      validMoves.push({ row: row - 1, col });
+      validMoves.push({ row: pieceRow - 1, col: pieceCol });
       break;
     case PieceType.ROOK:
-      for ({ row, col } of tiles[row]) {
-        validMoves.push({ row, col });
-      }
+      tiles.forEach((tileRow, i) => {
+        if (i === pieceRow) {
+          for (let { row, col } of tileRow) {
+            validMoves.push({ row, col });
+          }
+        } else {
+          validMoves.push({ row: i, col: pieceCol });
+        }
+      });
       break;
     case PieceType.KNIGHT:
-      validMoves.push({ row: row - 1, col });
+      validMoves.push({ row: pieceRow - 1, col: pieceCol });
       break;
     case PieceType.BISHOP:
-      validMoves.push({ row: row - 1, col });
+      validMoves.push({ row: pieceRow - 1, col: pieceCol });
       break;
     case PieceType.KING:
-      validMoves.push({ row: row - 1, col });
+      validMoves.push({ row: pieceRow - 1, col: pieceCol });
       break;
     case PieceType.QUEEN:
-      validMoves.push({ row: row - 1, col });
+      validMoves.push({ row: pieceRow - 1, col: pieceCol });
       break;
     default:
       break;
